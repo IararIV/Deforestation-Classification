ddea87adc6cde4173f52792005c0956e391f13b4
Pol Gràcia <pol.graciae@gmail.com>
2022-11-19 23:14:08 +0100

    Update timm.py

--- src/models/timm.py
+++ src/models/timm.py
@@ -72,5 +72,5 @@ class TimmModel(pl.LightningModule):
 
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=self.learning_rate)
-        scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=2, gamma=0.1, verbose=True)
+        scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.1, verbose=True)
         return {"optimizer": optimizer, "lr_scheduler": scheduler}
